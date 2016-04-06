ff54eb8d9fd8f17a59c24649a6adf2a2bc433997
houtaiw61@gmail.com <houtaiw61@gmail.com>
2016-04-06 17:52:57 -0400

    Adding feture to delete pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -20,7 +20,26 @@ class PicturesController < ApplicationController
     # render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
     end
   end
-  
+
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+    if @picture.update_attributes(picture_params)
+      redirect_to "/pictures/#{@picture.id}"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+    redirect_to pictures_url
+  end
+
   private
   def picture_params
     params.require(:picture).permit(:artist, :title, :url)
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,12 @@ Rails.application.routes.draw do
   post 'pictures' => 'pictures#create'
   get 'pictures/new' => 'pictures#new'
 
-
   get 'pictures/:id' => 'pictures#show', as: 'picture'
+
+  get 'pictures/:id/edit' => 'pictures#edit', as: 'edit_picture'
+  patch 'pictures/:id' => 'pictures#update'
+
+  delete 'pictures/:id' => 'pictures#destroy', as: 'delete_picture'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
